a4c090a01e193cab4a142e5e39e987107867c3f5
shaliginandrew <shaligin.andrew@gmail.com>
2020-11-11 07:45:40 +0300

    Перенос проекта Tracker в отдельный репозиторий

--- chapter_002/src/main/java/ru/job4j/tracker/Bug.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.tracker;
-
-public class Bug extends Item {
-
-    public Bug(String name) {
-        super(name);
-    }
-
-}
--- chapter_002/src/main/java/ru/job4j/tracker/UserAction.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.tracker;
-
-import java.sql.SQLException;
-
-public interface UserAction {
-    String name();
-
-    boolean execute(Input input, Store tracker) throws SQLException;
-}
